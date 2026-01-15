0a8f59f358e6b4c11ba5dc2e05b82ff84b0b16d1
江大爷 <84177566+baidu8@users.noreply.github.com>
2026-01-15 21:08:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,23 @@
 const CACHE_NAME = 'j-nav-v1';
-// 你想要离线也能访问的文件
 const ASSETS = [
   './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './data.js',
-  './icons/logo.svg'
+  'index.html',  // 确保仓库里文件名确实是这个
+  'style.css',
+  'script.js',
+  'data.js',
+  'icons/logo.svg'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS).catch(err => console.log('缓存失败的项目:', err));
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
